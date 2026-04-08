72bd7d1f75df28bc2a7a7cd30f9343c761fe85fd
amal <amaljw2002@gmail.com>
2026-04-08 21:09:44 +0200

    modified:   demo/src/main/java/com/example/demo/repository/ProgrammeRepository.java 	modified:   demo/src/main/java/com/example/demo/service/ProgrammeService.java

--- demo/src/main/java/com/example/demo/repository/ProgrammeRepository.java
+++ demo/src/main/java/com/example/demo/repository/ProgrammeRepository.java
@@ -1,6 +1,9 @@
 package com.example.demo.repository;
-package com.example.university.repository;
 
-public class ProgrammeRepository {
-    
+import com.example.demo.model.Programme;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProgrammeRepository extends JpaRepository<Programme, String> {
 }
--- demo/src/main/java/com/example/demo/service/ProgrammeService.java
+++ demo/src/main/java/com/example/demo/service/ProgrammeService.java
@@ -1,6 +1,43 @@
 package com.example.demo.service;
-package com.example.university.service;
 
+import com.example.demo.model.Programme;
+import com.example.demo.repository.ProgrammeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ProgrammeService {
     
+    @Autowired
+    private ProgrammeRepository programmeRepository;
+    
+    public List<Programme> getAllProgrammes() {
+        return programmeRepository.findAll();
+    }
+    
+    public Optional<Programme> getProgrammeById(String id) {
+        return programmeRepository.findById(id);
+    }
+    
+    public Programme createProgramme(Programme programme) {
+        return programmeRepository.save(programme);
+    }
+    
+    public Programme updateProgramme(String id, Programme programmeDetails) {
+        Programme programme = programmeRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Programme not found"));
+        
+        programme.setName(programmeDetails.getName());
+        programme.setDegree(programmeDetails.getDegree());
+        programme.setFaculty(programmeDetails.getFaculty());
+        programme.setMinEctsPerYear(programmeDetails.getMinEctsPerYear());
+        
+        return programmeRepository.save(programme);
+    }
+    
+    public void deleteProgramme(String id) {
+        programmeRepository.deleteById(id);
+    }
 }
